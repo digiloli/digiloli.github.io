939d049ed78c212c89458391ca0864c49b1e1395
digiloli <hgrotta@hotmail.com>
2022-03-11 18:19:18 -0300

    Updating

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 let lolidata = [];
+let brawl = [];
+let brawlOrdered = [];
 
 fetch("data/lolidata.json")
 .then(response => response.json())
@@ -10,6 +12,25 @@ for(let i = 1; i < lolidata.length; i++) {
     opt.innerHTML = lolidata[i]['name'] + " (" + lolidata[i]['display-name'] + ")";
     opt.value = "loli" + i;
     sel.appendChild(opt);
+}
+});
+
+fetch("data/ML_Heroes_Brawl.json")
+.then(response => response.json())
+.then(brawlx => {
+brawl = brawlx;
+let sel1 = document.getElementById('select-hero-1');
+let sel2 = document.getElementById('select-hero-2');
+for(let i = 0; i < brawl.length; i++) {
+	brawlOrdered = [... brawl].sort();
+    let opt1 = document.createElement('option');
+	let opt2 = document.createElement('option');
+    opt1.innerHTML = brawlOrdered[i];
+	opt2.innerHTML = brawlOrdered[i];
+    opt1.value = "hero1-" + i;
+	opt2.value = "hero2-" + i;
+    sel1.appendChild(opt1);
+	sel2.appendChild(opt2);
 }
 });
 
@@ -36,4 +57,18 @@ function DisplayLoli() {
 		<p>Esposa: ${esposa}</p>
 		</div>
 	`;
+}
+
+function DisplayHero() {
+	let hero1 = document.getElementById('select-hero-1');
+	let hero2 = document.getElementById('select-hero-2');
+	if (hero1.value==='hero1-none' || hero2.value==='hero2-none') {document.getElementById('hero-result').innerHTML = ``;return;}
+	hero1 = brawl.indexOf(brawlOrdered[(hero1.value).substr(6)]);
+	hero2 = brawl.indexOf(brawlOrdered[(hero2.value).substr(6)]);
+	if (hero1 < hero2) {
+		document.getElementById('hero-result').innerHTML = `<div><p>${brawl[hero1]}</p></div>`
+	}
+	else {
+		document.getElementById('hero-result').innerHTML = `<div><p>${brawl[hero2]}</p></div>`
+	}
 }
